1a1f9fe59ef066ca37e5572863dd289fd7dca4db
Siwat anandechsak <bright.siwat@hotmail.com>
2024-08-06 17:28:26 +0700

    refactor

--- excel_data.py
+++ excel_data.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import sys
 import os
+from datetime import datetime, time
+
 
 def get_data(lang: str):
     if lang == "EN":
@@ -11,21 +13,25 @@ def get_data(lang: str):
         return False
     return df.to_dict(orient='records')
 
+def format_dt(date_str:str,time_str:str):
+    # Parse the date string to a datetime object
+    date_obj = datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+
+    # Parse the time string to a time object
+    time_obj = datetime.strptime(time_str, "%H:%M:%S").time()
+
+    # Combine date and time into a single datetime object
+    merged_datetime = datetime.combine(date_obj.date(), time_obj)
+    return merged_datetime.strftime("%Y-%m-%d %H:%M:%S")
 if __name__ == '__main__':
-    excel_data = get_data("TH")
+    excel_data = get_data("EN")
     if excel_data == False:
         print("Exiting the program...")
         sys.exit("lang_type choose 'TH' or 'EN' !")
-    len_edit_btn = list(range(16,1,-1))
-    print(len_edit_btn)
-    for index in range(len(len_edit_btn)):
-        print(index)
-    print(f'/html/body/form/div[3]/div/table[1]/tbody/tr[{len_edit_btn[0]}]/td[13]/a[1]')
-    print(list(reversed(range(2,17))))
-    no = list(range(1, 21))
-
     for i in range(len(excel_data)):
-        if str(excel_data[i]['Link']) == "nan":
-            print(  excel_data[i]['Event_name']+ " " + " " + " " + str(excel_data[i]['start_time']) + " " + str(excel_data[i]['end_time']) + " " + str(no[i])+".jpg")
+        start_time = format_dt(str(excel_data[i]['start_date']),str(excel_data[i]['start_time']))
+        end_time = format_dt(str(excel_data[i]['end_date']),str(excel_data[i]['end_time']))
+        if str(excel_data[i]['link']) == "ไม่มีลิงก์":
+            print(  excel_data[i]['event_name']+ " " + " " + " " + start_time + " " + end_time+"\n" )
         else:
-            print(  excel_data[i]['Event_name']+ " " + str(excel_data[i]['Link'])  + " " + str(excel_data[i]['start_time']) + " " + str(excel_data[i]['end_time'])+ " " + str(no[i])+".jpg")
+            print(  excel_data[i]['event_name']+ " " + str(excel_data[i]['link'])  + " " + start_time + " " + end_time +"\n")
--- main.py
+++ main.py
@@ -5,14 +5,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from excel_data import get_data
+from excel_data import format_dt
 import time
 # from datetime import datetime
 import sys
 import os
 from dotenv import load_dotenv
 
+
 def main():
-    # Load environment variables from the .env file (if present)
+    """
+	|--------------------------------------------------------------------------
+	| Load environment variables from the .env file (if present)
+	|--------------------------------------------------------------------------
+    """
     load_dotenv()
 
     USERNAME = os.getenv('USERNAME_MIS')
@@ -22,14 +28,24 @@ def main():
     if USERNAME == None or PASSWORD == None:
         print("Exiting the program...")
         sys.exit("Missing env !")
-
-    # lang type - 'TH' or 'EN'
-    lang_type = "EN"
-
-    # number of edit button in page 
+    """
+	|--------------------------------------------------------------------------
+	| lang type - 'TH' or 'EN' and page 9 or 10
+	|--------------------------------------------------------------------------
+    """
+    lang_type = "TH"
+    page = 9
+    """
+	|--------------------------------------------------------------------------
+	| number of edit button in page 
+	|--------------------------------------------------------------------------
+    """
     arr_edit_btn = list(range(16,1,-1))
-
-    # get data from excel
+    """
+	|--------------------------------------------------------------------------
+	| get data from excel
+	|--------------------------------------------------------------------------
+    """
     excel_data = get_data(lang_type)
     if excel_data == False:
         print("Exiting the program...")
@@ -38,52 +54,76 @@ def main():
     img_no = list(range(1, 21))
     driver = setup_driver()
 
-
     login(driver,URL,USERNAME, PASSWORD)
-    navigate_to_ad_management(driver)
-    process_page(driver, excel_data, arr_edit_btn, img_no, lang_type)
-    
-
+    """
+	|--------------------------------------------------------------------------
+	| navigate_to_ad_management(driver)
+	|--------------------------------------------------------------------------
+    """
+    process_page(driver, excel_data, arr_edit_btn, img_no, lang_type,page)
     print("Finishing...")
     driver.quit()
 
 
-
+"""
+	|--------------------------------------------------------------------------
+	| Initialize the Chrome WebDriver
+	|--------------------------------------------------------------------------
+"""
 def setup_driver():
     chrome_options = Options()
     chrome_options.add_argument('--ignore-certificate-errors')
     chrome_options.add_argument('--ignore-ssl-errors')
 
-    # Initialize the Chrome WebDriver
     s = Service('C:/Users/MIS/Desktop/dev/chromedriver-win64/chromedriver.exe')
     return webdriver.Chrome(service=s,options=chrome_options)
 
 def login(driver,url,username,password):
     driver.get(url)
-    # Find the search box element and enter a query
+    """
+	|--------------------------------------------------------------------------
+	| Find the search box element and enter a query
+	|--------------------------------------------------------------------------
+    """
+    
     username_box = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/form/div[3]/div[1]/ul/li[1]/dl/dd/input')))
     username_box.send_keys(username)
     pw_box = driver.find_element(By.XPATH,'/html/body/form/div[3]/div[1]/ul/li[2]/dl/dd/input')
     pw_box.send_keys(password)
 
     time.sleep(6)
-
-    # Wait for the login button to be clickable and click it
+    """
+	|--------------------------------------------------------------------------
+	| Wait for the login button to be clickable and click it
+	|--------------------------------------------------------------------------
+    """
+    
     login_btn = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/form/div[3]/div[1]/ul/li[4]/input')))
     login_btn.click()
 
 def navigate_to_ad_management(driver):
-    # Nav
+    """
+	|--------------------------------------------------------------------------
+	| Nav
+	|--------------------------------------------------------------------------
+    """
+    
     iframe_left = driver.find_element(By.ID, "left")
-
-    # switch to selected iframe
+    """
+	|--------------------------------------------------------------------------
+	| switch to selected iframe
+	|--------------------------------------------------------------------------
+    """
+    
     driver.switch_to.frame(iframe_left)
-
-    # click on AdvertisingManagement button
+    """
+	|--------------------------------------------------------------------------
+	| click on AdvertisingManagement button
+	|--------------------------------------------------------------------------
+    """
     driver.find_element(By.ID, '25').click()
     driver.switch_to.default_content()
 
-    time.sleep(2)
 
 
 def get_image_path(index, lang_type):
@@ -92,66 +132,119 @@ def get_image_path(index, lang_type):
     return os.path.abspath(os.path.join(os.path.dirname(__file__), folder, file_name))
 
 
-def process_page(driver, excel_data, arr_edit_btn, img_no, lang_type):
-    # Main
-    iframe_main = driver.find_element(By.ID, "main")
-    driver.switch_to.frame(iframe_main)
+def process_page(driver, excel_data, arr_edit_btn, img_no, lang_type,page):
 
+    len_arr_edit_btn = len(arr_edit_btn)
 
-    for index in range(len(arr_edit_btn)):
+    for index in range(len_arr_edit_btn):
+        navigate_to_ad_management(driver)
+        """
+        |--------------------------------------------------------------------------
+        | ifram main
+        |--------------------------------------------------------------------------
+        """
+        iframe_main = driver.find_element(By.ID, "main")
+        driver.switch_to.frame(iframe_main)
         print(f"round : {index+1}")
         if lang_type == "EN":
             driver.find_element(By.XPATH,"/html/body/form/div[3]/div/div/dl/dd[6]/select/option[2]").click()
 
-        # go to page 10 
-        driver.find_element(By.XPATH, '/html/body/form/div[3]/div/table[2]/tbody/tr/td/div/a[9]').click()
-
-        # click on Edit button 
+        if page == 9:
+            """
+            |--------------------------------------------------------------------------
+            | navigate to page 9 
+            |--------------------------------------------------------------------------
+            """
+            # 
+            driver.find_element(By.XPATH, '/html/body/form/div[3]/div/table[2]/tbody/tr/td/div/a[8]').click()
+        else:
+            """
+            |--------------------------------------------------------------------------
+            | navigate to page 10
+            |--------------------------------------------------------------------------
+            """
+            #
+            driver.find_element(By.XPATH, '/html/body/form/div[3]/div/table[2]/tbody/tr/td/div/a[9]').click()
+        """
+        |--------------------------------------------------------------------------
+        | click on Edit button 
+        |--------------------------------------------------------------------------
+        """
         driver.find_element(By.XPATH, f'/html/body/form/div[3]/div/table[1]/tbody/tr[{arr_edit_btn[index]}]/td[13]/a[1]').click()
 
         fill_form(driver, excel_data[index], img_no[index], lang_type)
 
 def fill_form(driver, data, img_no, lang_type):
-    # title input
+        start_time = format_dt(str(data['start_date']),str(data['start_time']))
+        end_time = format_dt(str(data['end_date']),str(data['end_time']))
+        """
+        |--------------------------------------------------------------------------
+        | title input
+        |--------------------------------------------------------------------------
+        """
         title = driver.find_element(By.XPATH,"/html/body/form/div[3]/div/div/ul/li[2]/input")
         title.clear()
-        title.send_keys(data['Event_name'])
+        title.send_keys(data['event_name'])
+        """
+        |--------------------------------------------------------------------------
+        | link input
+        |--------------------------------------------------------------------------
+        """
 
-        # link input
-        if str(data['Link']) == "nan":
+        if str(data['link']) == "ไม่มีลิงก์":
             link = driver.find_element(By.XPATH,"/html/body/form/div[3]/div/div/ul/li[4]/input")
             link.clear()
         else:
             link = driver.find_element(By.XPATH,"/html/body/form/div[3]/div/div/ul/li[4]/input")
             link.clear()
-            link.send_keys(data['Link'])
-
-        # image_upload input
+            link.send_keys(data['link'])
+        """
+        |--------------------------------------------------------------------------
+        | image_upload input
+        |--------------------------------------------------------------------------
+        """
         image_path = get_image_path(img_no, lang_type)
         image_upload = driver.find_element(By.XPATH,"/html/body/form/div[3]/div/div/ul/li[3]/input[1]")
         image_upload.send_keys(image_path)
-
-        # check file uploaded name
+        """
+        |--------------------------------------------------------------------------
+        | check file uploaded name
+        |--------------------------------------------------------------------------
+        """
         assert os.path.basename(image_path) == str(img_no)+".jpg"
-
-        # start time input
+        """
+        |--------------------------------------------------------------------------
+        | start time input
+        |--------------------------------------------------------------------------
+        """
         start_time = driver.find_element(By.XPATH,"/html/body/form/div[3]/div/div/ul/li[7]/input")
         start_time.clear()
-        start_time.send_keys(str(data['start_time']))
-
-        # end time input
+        start_time.send_keys(start_time)
+        """
+        |--------------------------------------------------------------------------
+        | end time input
+        |--------------------------------------------------------------------------
+        """
         end_time = driver.find_element(By.XPATH,"/html/body/form/div[3]/div/div/ul/li[8]/input")
         end_time.clear()
-        end_time.send_keys(str(data['end_time']))
+        end_time.send_keys(end_time)
 
-        time.sleep(2)
-        # submit btn 
+        """
+        |--------------------------------------------------------------------------
+        | submit button 
+        |--------------------------------------------------------------------------
+        """
         driver.find_element(By.XPATH, '/html/body/form/div[3]/div/div/ul/li[21]/input[1]').click()
-        time.sleep(2)
-        # accept alert
+
+        """
+        |--------------------------------------------------------------------------
+        | accept alert 
+        |--------------------------------------------------------------------------
+        """
         WebDriverWait(driver, 3).until(EC.alert_is_present())
         driver.switch_to.alert.accept()
-        navigate_to_ad_management(driver)
+        driver.switch_to.default_content()
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
